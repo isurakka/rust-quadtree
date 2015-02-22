0703e60a924cb05ca8d44a8ea7beb0b383a82df7
Iiro Surakka <iiro.surakka@gmail.com>
2015-02-22 17:51:26 +0200

    Add first set function and first test

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,30 @@ use std::option::{Option};
 use std::cell::{RefCell};
 use std::rc::{Rc, Weak};
 use std::num::{Int, UnsignedInt};
+use std::ops::{Sub};
 
-struct AABB2<T> {
+#[derive(Copy)]
+struct AABB2<T> 
+    where T: Copy + Sub<T> {
     lower_bound: Vec2<T>,
     upper_bound: Vec2<T>,
 }
 
-impl<T> AABB2<T> {
+impl<T> AABB2<T>
+    where T: Copy + Sub<T> {
     fn new(lower_bound: Vec2<T>, upper_bound: Vec2<T>) -> AABB2<T> {
         AABB2 {
             lower_bound: lower_bound,
             upper_bound: upper_bound,
         }
+    }
+
+    pub fn get_width(&self) -> <T as Sub>::Output {
+        self.upper_bound.x - self.lower_bound.x
+    }
+
+    pub fn get_height(&self) -> <T as Sub>::Output {
+        self.upper_bound.y - self.lower_bound.y
     }
 }
 
@@ -64,16 +76,20 @@ enum NodeType {
 
 impl<'a, T> RegionQuadtree<'a, T>
     where T: Copy + PartialEq {
-    pub fn new(resolution: u32) -> RegionQuadtree<'a, T> {
+    pub fn new(resolution: u32, initial_value: Option<T>) -> Option<RegionQuadtree<'a, T>> {
         let size = RegionQuadtree::<T>::calculate_size(resolution);
-        RegionQuadtree {
+        if (size.is_none())
+        {
+            return None;
+        }
+        Some(RegionQuadtree {
             resolution: resolution,
             depth: 0,
-            node: Node::Empty,
-            aabb: AABB2::new(Vec2::new(0, 0), Vec2::new(size, size)),
+            node: initial_value.map_or(Node::Empty, |v| Node::Full(v)),
+            aabb: AABB2::new(Vec2::new(0, 0), Vec2::new(size.unwrap(), size.unwrap())),
             parent: None,
             event_handler: None,
-        }
+        })
     }
 
     fn new_child(resolution: u32, depth: u32, value: Option<T>, parent: &'a RegionQuadtree<'a, T>, aabb: AABB2<u32>) 
@@ -94,16 +110,28 @@ impl<'a, T> RegionQuadtree<'a, T>
         }
     }
 
-    fn calculate_size(resolution: u32) -> u32 {
-        (2 as u32).pow(resolution as usize)
+    fn calculate_size(resolution: u32) -> Option<u32> {
+        let size = (2 as u32).pow(resolution as usize);
+        match size {
+            0 => None,
+            _ => Some(size)
+        }
     }
 
     fn get_type(&self) -> (NodeType, Option<T>) {
         match self.node {
             Node::Children(_, _, _, _) => (NodeType::Grey, None),
-            Node::Full(value) => (NodeType::Black, value),
+            Node::Full(value) => (NodeType::Black, Some(value)),
             Node::Empty => (NodeType::White, None),
         }
+    }
+
+    fn get_resolution(&self) -> u32 {
+        self.resolution
+    }
+
+    fn get_aabb(&self) -> AABB2<u32> {
+        self.aabb
     }
 
     fn propagate_event(&self, event_type: EventType, start: Option<&RegionQuadtree<'a, T>>, old_value: Option<T>)
@@ -135,7 +163,7 @@ impl<'a, T> RegionQuadtree<'a, T>
         }
     }
 
-    pub fn set(&mut self, value: T) -> bool {
+    pub fn set(&mut self, value: Option<T>) -> bool {
         let anySet = self.set_internal(value);
         if (anySet)
         {
@@ -144,34 +172,60 @@ impl<'a, T> RegionQuadtree<'a, T>
         anySet
     }
 
-    fn set_internal(&mut self, value: Option<T>) -> bool {
-        let black = match self.node {
-            Node::Full(myValue) => Some(myValue),
-            _ => None
-        };
+    fn set_internal(&mut self, opt_value: Option<T>) -> bool {
+        match opt_value.is_some() {
+            true => {
+                let value = opt_value.unwrap();
+                let black = match self.node {
+                    Node::Full(v) => Some(v),
+                    _ => None
+                };
 
-        if (!black.is_none()) {
-            let old_value = black.unwrap();
-            if (old_value == value) {
-                return false;
-            } else {
+                if (!black.is_none()) {
+                    let old_value = black.unwrap();
+                    if (old_value == value) {
+                        return false;
+                    } else {
+                        self.node = Node::Full(value);
+                        self.propagate_event(EventType::Changed, None, Some(old_value));
+                        return true;
+                    }
+                }
+
+                //unset();
+
                 self.node = Node::Full(value);
-                self.propagate_event(EventType::Changed, None, Some(old_value));
-                return true;
-            }
-        }
 
-        //unset();
+                self.propagate_event(EventType::Added, None, None);
 
-        self.node = Node::Full(value);
+                return true
+            },
+            false => { 
 
-        self.propagate_event(EventType::Added, None, None);
-
-        true
+            },
+        };
+        
+        false
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::RegionQuadtree;
+
+    #[test]
+    fn new_different_resolutions()
+    {
+        let qt1 = RegionQuadtree::new(3, Some(3u32)).unwrap();
+        assert_eq!(8, qt1.get_aabb().get_width());
+
+        let qt2 = RegionQuadtree::new(-1, Some(3u32));
+        assert!(qt2.is_none());
+
+        let qt3 = RegionQuadtree::new(31, Some(3u32));
+        assert!(qt3.is_some());
+
+        let qt4 = RegionQuadtree::new(32, Some(3u32));
+        assert!(qt4.is_none());
+    }
 }
