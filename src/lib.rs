c2cec38f66a96019cf4514d69fb7c58298fe0713
Iiro Surakka <iiro.surakka@gmail.com>
2015-02-23 23:35:38 +0200

    New approach progress

--- src/lib.rs
+++ src/lib.rs
@@ -37,24 +37,10 @@ trait RegionQuadtreeEventHandler<T> {
     fn on_changed(&self, quadtree: &RegionQuadtree<T>);
 }
 
-pub enum Node<'a, T: 'a> 
-    where T: Copy + PartialEq
-{
-    // Has four quadrants
-    Children(RefCell<[Option<Weak<RegionQuadtree<'a, T>>>; 4]>),
-    // Leaf with value
-    Full(T),
-    // Empty leaf
-    Empty,
-}
-
 pub struct RegionQuadtree<'a, T: 'a>
     where T: Copy + PartialEq {
+    nodes: Vec<Option<T>>,
     resolution: u32,
-    depth: u32,
-    node: Node<'a, T>,
-    aabb: AABB2<u32>,
-    parent: Option<Rc<&'a RegionQuadtree<'a, T>>>,
     event_handler: Option<&'a (RegionQuadtreeEventHandler<T> + 'a)>,
 }
 
@@ -80,148 +66,40 @@ impl<'a, T> RegionQuadtree<'a, T>
         }
         Some(RegionQuadtree {
             resolution: resolution,
-            depth: 0,
-            node: initial_value.map_or(Node::Empty, |v| Node::Full(v)),
-            aabb: AABB2::new(Vec2::new(0, 0), Vec2::new(size.unwrap(), size.unwrap())),
-            parent: None,
+            nodes: vec![None, size],
             event_handler: None,
         })
     }
 
-    fn new_child(resolution: u32, depth: u32, value: Option<T>, 
-        parent: Rc<&'a RegionQuadtree<'a, T>>, aabb: AABB2<u32>) 
-        -> RegionQuadtree<'a, T> 
-    {
-        let node = match value {
-            Some(v) => Node::Full(v),
-            None => Node::Empty,
-        };
-
-        RegionQuadtree {
-            resolution: resolution,
-            depth: depth,
-            node: node,
-            aabb: aabb,
-            parent: Some(parent),
-            event_handler: None,
-        }
-    }
-
     fn calculate_size(resolution: u32) -> Option<u32> {
-        let size = (2 as u32).pow(resolution as usize);
-        match size {
-            0 => None,
-            _ => Some(size)
-        }
-    }
+        let size = 4 * resolution + 4;
 
-    fn get_type(&self) -> (NodeType, Option<T>) {
-        match self.node {
-            Node::Children(_) => (NodeType::Grey, None),
-            Node::Full(value) => (NodeType::Black, Some(value)),
-            Node::Empty => (NodeType::White, None),
+        if (size <= 0)
+        {
+            return None;
         }
+        
+        Some(size)
     }
 
     fn get_resolution(&self) -> u32 {
         self.resolution
     }
 
-    fn get_aabb(&self) -> AABB2<u32> {
+    fn get_aabb(&self, index: usize) -> AABB2<u32> {
         self.aabb
     }
 
-    fn propagate_event(&self, event_type: EventType, start: Option<&RegionQuadtree<'a, T>>, old_value: Option<T>)
+    fn propagate_event(&self, event_type: EventType, 
+        start: Option<&RegionQuadtree<'a, T>>, old_value: Option<T>)
     {
-        let always = match start {
-            Some(v) => v,
-            None => self
-        };
-
-        let par_opt = match start {
-            Some(v) => Some(v),
-            None => Some(self)
-        };
-
-        while !par_opt.is_none() {
-            let par = &par_opt.unwrap();
-            if par.event_handler.is_none() {
-                continue;
-            }
-            let event_handler = par.event_handler.unwrap();
-
-            match event_type {
-                EventType::Added    => event_handler.on_added(always),
-                EventType::Removing => event_handler.on_removing(always),
-                EventType::Changed  => event_handler.on_changed(always),
-            }
-
-            let par_opt = &par.parent;
-        }
+        
     }
 
     pub fn set(&mut self, value: Option<T>) -> bool {
-        let anySet = self.set_internal(value);
-        if (anySet)
-        {
-            //unsubdivide()
-        }
-        anySet
-    }
-
-    fn set_internal(&mut self, opt_value: Option<T>) -> bool {
-        let black = match self.node {
-            Node::Full(v) => Some(v),
-            _ => None
-        };
-
-        match opt_value.is_some() {
-            true => {
-                let value = opt_value.unwrap();
-
-                if (black.is_some()) {
-                    let old_value = black.unwrap();
-                    if (old_value == value) {
-                        return false;
-                    } else {
-                        self.node = Node::Full(value);
-                        self.propagate_event(EventType::Changed, None, Some(old_value));
-                        return true;
-                    }
-                }
-
-                //unset();
-                self.node = Node::Full(value);
-                self.propagate_event(EventType::Added, None, None);
-
-                return true
-            },
-            false => { 
-                if (black.is_some()) {
-                    self.propagate_event(EventType::Removing, None, None);
-                    self.node = Node::Empty;
-                    return true;
-                }
-
-                match self.node {
-                    Node::Empty => return false,
-                    _ => (),
-                };
-
-                match self.node {
-                    Node::Children(ref mut children) => {
-                        let mut any = false;
-                        for c in children.borrow_mut().iter_mut() {
-                            any |= std::rc::get_mut(&mut c.unwrap().upgrade().unwrap()).unwrap().set_internal(opt_value);
-                        }
-                        return any;
-                    },
-                    _ => ()
-                };
-            },
-        };
         
-        panic!("No matches while setting a value");
+        
+        false
     }
 
     fn subdivide(&'a mut self) -> bool {
